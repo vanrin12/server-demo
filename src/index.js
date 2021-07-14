f202f8b5cb53aa8300dbfa3061017c7dc951356c
Rin To <rin.to@ enjoyworks.co.kr>
2021-07-14 09:27:59 +0700

    Create server demo with apollo server

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schemas");
+const resolvers = require("./resolvers");
+const { products, categories, customers, carts } = require("./datasets");
+
+const initLocalstorage = () => {
+  if (typeof localStorage === "undefined" || localStorage === null) {
+    var LocalStorage = require("node-localstorage").LocalStorage;
+    localStorage = new LocalStorage("./scratch");
+  }
+  localStorage.setItem("products", JSON.stringify(products));
+  localStorage.setItem("categories", JSON.stringify(categories));
+  localStorage.setItem("customers", JSON.stringify(customers));
+  localStorage.setItem("carts", JSON.stringify(carts));
+};
+
+initLocalstorage();
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
---
+++ src/schemas.js
@@ -0,0 +1,123 @@
+const { gql } = require("apollo-server");
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  # ENUMS
+  enum Colors {
+    White
+    Black
+    Pink
+    Red
+    Yellow
+    Blue
+    Green
+  }
+  enum Sizes {
+    XS
+    S
+    M
+    L
+    XL
+    XXL
+  }
+  # SCHEMAS
+  type Product {
+    id: ID
+    name: String
+    quantity: Int
+    price: Float
+    thumbnail: String
+    description: String
+    category: Category
+    size: Sizes
+    color: Colors
+  }
+  type Category {
+    id: ID
+    name: String
+    description: String
+  }
+  type Cart {
+    id: ID
+    product: Product
+    quantity: Int
+    customer: Customer
+  }
+  type Customer {
+    id: ID
+    username: String
+    first_name: String
+    last_name: String
+    avatar_url: String
+    gender: Boolean # true: male, false: female
+  }
+  # INPUT PARAM
+  input productInput {
+    name: String
+    quantity: Int
+    price: Float
+    thumbnail: String
+    description: String
+    category: Int
+    size: String
+    color: String
+  }
+  input customerInput {
+    username: String
+    first_name: String
+    last_name: String
+    avatar_url: String
+    gender: Boolean # true: male, false: female
+  }
+  input cartInput {
+    product: Int
+    quantity: Int
+    customer: Int
+  }
+  # RESPONSES
+  interface MutationResponse {
+    code: String!
+    success: Boolean!
+    message: String!
+  }
+  type ProductMutationResponse implements MutationResponse {
+    code: String!
+    success: Boolean!
+    message: String!
+    product: Product
+  }
+  type CustomerMutationResponse implements MutationResponse {
+    code: String!
+    success: Boolean!
+    message: String!
+    customer: Customer
+  }
+  type CartMutationResponse implements MutationResponse {
+    code: String!
+    success: Boolean!
+    message: String!
+    cart: Cart
+  }
+  # QUERIES
+  type Query {
+    getProducts: [Product]
+    getCategories: [Category]
+    getProduct(id: String!): Product
+    getCustomer(id: String!): Customer
+    getCart(id: String!): Cart
+  }
+  # MUTATIONS
+  type Mutation {
+    addProduct(input: productInput!): ProductMutationResponse
+    updateProduct(id: String!, input: productInput!): ProductMutationResponse
+    deleteProduct(id: String!): ProductMutationResponse
+    addCustomer(input: customerInput): CustomerMutationResponse
+    updateCustomer(id: String!, input: customerInput): CustomerMutationResponse
+    addCart(input: cartInput): CartMutationResponse
+    updateCart(id: String!, input: cartInput): CartMutationResponse
+  }
+`;
+
+module.exports = typeDefs;
